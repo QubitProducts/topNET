63c79b4ea4bca3d74863666dd22e91a9ce99891d
Peter Fronc <peter.fronc@qubit.com>
2016-08-18 14:49:51 +0100

    Adding readme.

--- src/main/java/com/qubit/qurricane/MainAcceptAndDispatchThread.java
+++ src/main/java/com/qubit/qurricane/MainAcceptAndDispatchThread.java
@@ -10,7 +10,6 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
