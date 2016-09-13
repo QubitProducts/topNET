458e90d372a4d7a39b12ef81b50f889b9f0c7ef2
Peter Fronc <peter.fronc@qubit.com>
2016-09-13 17:08:26 +0100

    Adding demo.

---
+++ src/main/java/com/qubit/qurricane/examples/Demo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qubit.qurricane.examples;
+
+import static com.qubit.qurricane.Handler.registerHandlerByPath;
+import com.qubit.qurricane.Server;
+import java.io.IOException;
+
+/**
+ *
+ * @author Peter Fronc <peter.fronc@qubitdigital.com>
+ */
+public class Demo {
+  
+  /**
+   * Example main.
+   * @param args
+   * @throws IOException 
+   */
+  public static void main(String[] args) throws IOException {    
+    Server s = new Server("localhost", 3456);
+    
+    s.setJobsPerThread(4);
+    // one byte buffer!
+    s.setThreadsAmount(16);
+    s.start();
+    
+    registerHandlerByPath("/echo", new EchoHandler());
+    registerHandlerByPath("/appender", new AsyncAppenderHandler());
+  }
+}
