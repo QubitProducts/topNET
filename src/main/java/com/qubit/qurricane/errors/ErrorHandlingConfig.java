227bb26804fe7a23cb51aaddefedd03d19413ff9
Peter Fronc <peter.fronc@qubit.com>
2016-11-01 17:01:14 +0000

    Adding LGPL license.

--- src/main/java/com/qubit/qurricane/errors/ErrorHandlingConfig.java
+++ src/main/java/com/qubit/qurricane/errors/ErrorHandlingConfig.java
@@ -1,7 +1,7 @@
 /*
  * Qurrican
  * Fast HTTP Server Solution.
- * Copyright 2016, Qubit Group
+ * Copyright 2016, Qubit Group <www.qubit.com>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
