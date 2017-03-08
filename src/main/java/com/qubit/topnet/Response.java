ec155e0157b532c18c81283d060121ba0ef9d205
Peter Fronc <peter.fronc@qubit.com>
2017-03-08 15:46:56 +0000

    Generalizing version number.

--- src/main/java/com/qubit/topnet/Response.java
+++ src/main/java/com/qubit/topnet/Response.java
@@ -42,10 +42,8 @@ import java.util.logging.Logger;
  */
 public class Response {
   static private final Logger log = Logger.getLogger(Response.class.getName());
-  
-  public static String SERVER_VERSION = "2";
-  
-  public static String serverName = "topNET" + "/" + SERVER_VERSION;
+    
+  public static String serverName = "topNET/2.0";
   
   private static final String CRLF = "\r\n";
   private static final String OK_200 = "200 OK" + CRLF;
