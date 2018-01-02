4390ce136192d02008491a7a8da55daba4c02ece
Peter Fronc <peter.fronc@qubit.com>
2018-01-02 17:16:42 +0000

    Version at 2.1.13

--- src/main/java/com/qubit/topnet/Response.java
+++ src/main/java/com/qubit/topnet/Response.java
@@ -45,7 +45,7 @@ import java.util.logging.Logger;
 public class Response {
   static private final Logger log = Logger.getLogger(Response.class.getName());
 
-  public static String serverName = "topNET/2.0.12";
+  public static String serverName = "topNET/2.0.13";
   
   private static final String CRLF = "\r\n";
   private static final String OK_200 = "200 OK" + CRLF;
